02364d6b09ff04115e4c5479af57f34371dd7637
Eungyeol41 <iniziato@naver.com>
2021-08-23 22:47:12 +0900

    왜 module 오류지....

--- nodejs_004/routes/bbsRouter.js
+++ nodejs_004/routes/bbsRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { UPDATE } = require("sequelize/types/lib/query-types");
 const router = express.Router();
 
 const { tbl_bbs } = require("../models/index");
@@ -17,14 +18,13 @@ router.get("/update", (req, res) => {
   console.log("seq 값은?", seq);
 
   tbl_bbs.findByPk(seq).then((result) => {
-    console.log("findById", result);
+    console.log("findByPk", result);
     res.render("update", { BBS: result });
   });
 });
-router.post("/update", (req, res) => {
-  tbl_bbs.create(req.body).then((result) => {
-    res.json(result);
-  });
-});
+// router.post("/update", (req, res) => {
+//   const b_id = req.query.b_id;
+//   req.body.b_id = b_id;
+// });
 
 module.exports = router;
