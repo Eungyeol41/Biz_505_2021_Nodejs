8652411a5b8b9c970bf5e63b5ca33317b801986d
Eungyeol41 <iniziato@naver.com>
2021-08-28 23:13:07 +0900

    테이블 번호 넘김....

--- pos/routes/posRouter.js
+++ pos/routes/posRouter.js
@@ -3,28 +3,17 @@ const router = express.Router();
 const { tbl_orders, tbl_products } = require("../models/index");
 
 router.get("/detail", (req, res) => {
-  const o_table = req.query.o_table;
-  console.log("o_table", o_table);
-
-  tbl_orders.findOne({ where: { o_table } }).then((result) => {
-    if (result) {
-      res.render("detail", { BBS: result });
-    } else {
-      res.render("order", "");
-    }
-  });
+  const {o_table} = req.query;
+  console.log("detail-o_table", {o_table});
 });
 
 router.get("/list", (req, res) => {
-  console.log("query값", req.query);
-  const p_code = req.query.p_code;
-  console.log("p_code", p_code);
-
-  res.render("order");
-
-  //   tbl_products.findOne({ where: { p_code }, include: { models: tbl_products } }).then((result) => {
-  //     res.render("order", { BBS: result });
-  //   });
+  const {o_table} = req.query
+  
+  tbl_orders.findAndCountAll().then(result=>{
+	  res.render("order", {ORDER:result.rows, o_table: o_table})
+  })
+  
 });
 
 router.get("/pay", (req, res) => {
