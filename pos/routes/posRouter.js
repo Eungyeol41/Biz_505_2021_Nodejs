d03d6fb76857c9157a09180fda8c6777585f1f91
Eungyeol41 <iniziato@naver.com>
2021-08-29 23:34:59 +0900

    Router 수정

--- pos/routes/posRouter.js
+++ pos/routes/posRouter.js
@@ -13,19 +13,11 @@ router.get("/list", (req, res) => {
   tbl_products.findAndCountAll().then(result=>{
 	  res.render("order", {PRODUCT:result.rows, o_table: o_table})
   })
-	// tbl_products.findAll({ include : [{model : tbl_orders}] }).then(result=>{
-	// 	res.render("order", {PRODUCT:result, o_table: o_table})
-	// })
+
 });
 
 router.get("/pay", (req, res) => {
   res.render("pay");
 });
-
-router.get("/listPug", (req, res) => {
-	tbl_products.findAndCountAll().then(result=>{
-		res.render("list", {vo : result.rows});
-	});
-})
 
 module.exports = router;
