667f9ade2aaa362030e5dfb56b41e79bcd230e8e
Eungyeol41 <iniziato@naver.com>
2021-08-30 09:15:19 +0900

    흠

--- pos/routes/posRouter.js
+++ pos/routes/posRouter.js
@@ -3,17 +3,27 @@ const router = express.Router();
 const { tbl_orders, tbl_products } = require("../models/index");
 
 router.get("/detail", (req, res) => {
-  const {o_table} = req.query;
-  console.log("detail-o_table", {o_table});
+  const { o_table } = req.query;
+  console.log("detail-o_table", { o_table });
 });
 
 router.get("/list", (req, res) => {
-  const {o_table} = req.query
-  
-  tbl_products.findAndCountAll().then(result=>{
-	  res.render("order", {PRODUCT:result.rows, o_table: o_table})
-  })
+  const { o_table } = req.query;
+  const { table } = req.body;
 
+  tbl_products.findAll().then((result) => {
+    res.render("order", { PR: { table }, o_table: o_table });
+  });
+});
+router.post("/list", (req, res) => {
+  console.log("req의 body는?", req.body);
+  const p_name = req.body.p_name;
+  console.log("p_name", p_name);
+
+  const { table } = req.body;
+  tbl_orders.insert({ table }).then((result) => {
+    res.render("order", { table });
+  });
 });
 
 router.get("/pay", (req, res) => {
