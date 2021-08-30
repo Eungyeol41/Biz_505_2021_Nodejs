e43032e0da6b11a0f9574cbeb2c8ad59e797d734
Eungyeol41 <iniziato@naver.com>
2021-08-31 00:16:18 +0900

    detail.pug 수정

--- pos/routes/posRouter.js
+++ pos/routes/posRouter.js
@@ -5,6 +5,9 @@ const { tbl_orders, tbl_products } = require("../models/index");
 router.get("/detail", (req, res) => {
   const { o_table } = req.query;
   console.log("detail-o_table", { o_table });
+  tbl_orders.findAndCountAll().then((result) => {
+	  res.render("detail", { OR : result.rows, o_table : o_table });
+  })
 });
 
 router.get("/list", (req, res) => {
@@ -14,18 +17,8 @@ router.get("/list", (req, res) => {
     res.render("order", { PR: result.rows, o_table: o_table });
   });
 });
-router.post("/list", (req, res) => {
-  //   const p_name = req.body.p_name;
-  //   console.log("p_name", p_name);
-  //   const { table } = req.body;
-  //   tbl_orders.insert({ table }).then((result) => {
-  //     res.render("order", { table });
-  //   });
-});
 
 router.get("/insert", (req, res) => {
-  //   console.log("없다구");
-  //   console.log("req의 body는?", req.query.p_code);
   let { p_code } = req.query;
   tbl_products.findByPk(p_code).then((result) => {
     // console.log("나오니", result.dataValues);
@@ -33,10 +26,6 @@ router.get("/insert", (req, res) => {
     // return result;
     res.json(result);
   });
-});
-
-router.get("/pay", (req, res) => {
-  res.render("pay");
 });
 
 module.exports = router;
