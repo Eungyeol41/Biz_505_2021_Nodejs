7371b43c5ad649f2ae731ed63af26dc305d9684e
Eungyeol41 <iniziato@naver.com>
2021-08-31 09:08:48 +0900

    합계 수정

--- pos/routes/index.js
+++ pos/routes/index.js
@@ -5,6 +5,19 @@ const { tbl_orders } = require("../models/index");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  const prefix = "2021";
+  const precode = "000000";
+
+  let b_code = prefix + precode;
+  let mcode = b_code.substring(4);
+  mcode = parseInt(mcode) + 1;
+  mcode = "00000" + mcode;
+  mcode = mcode.substring(mcode.length - 6);
+  console.log("mcode", mcode);
+
+  b_code = prefix + mcode;
+  console.log("b_code", b_code);
+
   tbl_orders.findAndCountAll().then((result) => {
     res.render("index", { OR: result.rows });
   });
